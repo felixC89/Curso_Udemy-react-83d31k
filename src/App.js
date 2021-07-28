646e932b48d398d46341dd777c3fbab2374bd2f7
felixC89 <felix.calderon89@hotmail.com>
2021-07-28 12:21:59 -0600

    Componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import HolaMundo, { AdiosMundo } from './Componentes/HolaMundo.js';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>
+        <HolaMundo />
+        Start editing to see some magic happen :)
+        <AdiosMundo />
+      </p>
     </div>
   );
 }
---
+++ src/Componentes/HolaMundo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default HolaMundo;
+
+function HolaMundo() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <h3>Felix Calderon</h3>
+    </div>
+  );
+}
+
+export function AdiosMundo() {
+  return (
+    <div>
+      <h1>Adios Mundo</h1>
+      <h3>Felix Antonio</h3>
+    </div>
+  );
+}
